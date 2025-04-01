ea9bed849f1469fa623eb789418c2955e7d943b6
mafutka <mafutka@gmail.com>
2025-04-01 13:33:27 +0300

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+
 export default defineConfig(({ command }) => {
   return {
+    base: "/goit-js-hw-10/",
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
