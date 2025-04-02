df1d3b0c53c422d5bda07da849c0b52dbce0a790
mafutka <mafutka@gmail.com>
2025-04-02 10:54:50 +0300

    new-build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'; 
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-
 export default defineConfig(({ command }) => {
   return {
-    base: "/goit-js-hw-10/",
+    base: "/goit-js-hw-10/", 
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
@@ -36,7 +35,7 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
+      outDir: 'docs', 
       emptyOutDir: true,
     },
     plugins: [
